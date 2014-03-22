bccd3152e3498265e211b4521c8bc354163b5900
katie zhu <to.kzhu@gmail.com>
2014-03-22 10:51:26 -0400

    data n stuff

---
+++ tweets.js
@@ -0,0 +1,26 @@
+var Twitter = require('twitter')
+var util = require('util')
+var fs = require('fs')
+
+var longUrl = 'http://www.nytimes.com/2014/03/16/magazine/silicon-valleys-youth-problem.html'
+var shortUrl = 'http://nyti.ms/1gp29ue'
+
+var twitterClient = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_SECRET
+})
+
+console.log(process.env.TWITTER_CONSUMER_KEY)
+console.log(process.env.TWITTER_CONSUMER_SECRET)
+console.log(process.env.TWITTER_ACCESS_KEY)
+console.log(process.env.TWITTER_ACCESS_SECRET)
+
+twitterClient.search(longUrl, function (data) {
+  fs.writeFileSync('longUrl.json', JSON.stringify(data))
+})
+
+twitterClient.search(shortUrl, function (data) {
+  fs.writeFileSync('shortUrl.json', JSON.stringify(data))
+})
